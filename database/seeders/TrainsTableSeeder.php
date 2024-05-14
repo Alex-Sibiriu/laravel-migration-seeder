4dd4f219e5602851b8bdd45b96c1e34ec8714bb0
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-05-14 14:49:36 +0200

    [add] Slug Control

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -16,7 +17,6 @@ class TrainsTableSeeder extends Seeder
     {
         for ($i = 0; $i < 50; $i++) {
             $new_train = new Train();
-            $new_train->slug = $faker->slug();
             $new_train->company = $faker->name();
             $new_train->departure_station = $faker->words(3, true);
             $new_train->arrival_station = $faker->words(3, true);
@@ -24,8 +24,24 @@ class TrainsTableSeeder extends Seeder
             $new_train->arrival_time = $faker->time();
             $new_train->train_code = $faker->numberBetween(100000000000, 999999999999);
             $new_train->carriages_number = $faker->randomDigit();
+            $new_train->slug = $this->generateSlug($new_train->company, $new_train->train_code);
 
             $new_train->save();
         }
+    }
+    private function generateSlug($string, $code)
+    {
+        $slug = Str::slug($string . '-' . $code, '-');
+        $original_slug = $slug;
+
+        $exists = Train::find($slug);
+        $c = 1;
+
+        while ($exists) {
+            $slug = $original_slug . '-' . $c;
+            $exists = Train::find($slug);
+            $c++;
+        }
+        return $slug;
     }
 }
